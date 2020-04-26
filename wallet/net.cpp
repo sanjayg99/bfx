a8dc0f85ff64f0ea7296c7d6ac7f044d5f375e63
bfxcrypto <64330455+bfxcrypto@users.noreply.github.com>
2020-04-25 17:55:58 -0700

    Update net.cpp

--- wallet/net.cpp
+++ wallet/net.cpp
@@ -1240,7 +1240,7 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-    {"seed", "seed.nebl.io"}
+    {"seed", "seed.bfxworld.com"}
 };
 
 void ThreadDNSAddressSeed(void* parg)
